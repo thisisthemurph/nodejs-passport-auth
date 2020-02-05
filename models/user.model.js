9640404a1e3c7eaa9a37ffbeecaee5ee65464d6d
Michael Murphy <michael.w.murphy@outlook.com>
2020-02-05 21:01:37 +0000

    Updated the user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,23 @@
 const mongoose = require('mongoose')
 
 const UserSchema = mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
+    name: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 255
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 255
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 1024
+    }
 }, { timestamps: true })
 
 module.exports = mongoose.model('User', UserSchema)
