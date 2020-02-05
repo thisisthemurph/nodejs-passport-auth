d084e0f6a551d3fe1f62df8322f274b10375a5e0
Michael Murphy <michael.w.murphy@outlook.com>
2020-02-05 21:00:53 +0000

    Updated the database connection settings

--- db.js
+++ db.js
@@ -1,17 +1,14 @@
+const dotenv = require('dotenv')
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
+
+dotenv.config()
 
 const dbConfig = require('./config/db.config.js')
 
 module.exports.connect = () => {
-    mongoose.connect(dbConfig.uri, {
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-        useUnifiedTopology: true
-    })
+    mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true })
         .then(() => console.log('Connected to the database...'))
-        
         .catch(err => {
             console.error('Could not connect to the database!')
             console.error(err)
