d8dfbf925789198cb8d13bfd551dcbb652f811f9
Michael Murphy <michael.w.murphy@outlook.com>
2020-02-05 22:10:17 +0000

    Added password hashing

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const bcrypt = require('bcryptjs')
 const UserModel = require('../models/user.model.js')
 const validation = require('../validation.js')
 
@@ -8,10 +9,18 @@ router.post('/register', async (req, res) => {
     const {error} = validation.registrationUserValidaion(req.body)
     if (error) return res.status(400).send(error.details[0].message)
 
+    // Does the user already exist
+    const emailExists = await UserModel.findOne({ email: req.body.email })
+    if (emailExists) return res.status(400).send('A user with that email address already exists')
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(req.body.password, salt)
+
     const user = new UserModel({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hash
     })
     
     try {
