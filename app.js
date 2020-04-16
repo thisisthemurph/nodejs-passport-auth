272943087d00d3069c033b497c60787c302e5b40
= <michael.w.murphy@outlook.com>
2020-04-16 14:42:35 +0100

    Allowed cross origin CORS for testing purposes

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ require('./db.js').connect()
 // Middleware
 app.use(express.json())
 
+// Allow cross origin CORS for testing
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "http://localhost:3000") // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, auth-token")
+	next();
+});
+
 // Routes
 app.use('/api', require('./routes/index.routes.js'))
 app.use('/api/auth', require('./routes/auth.routes.js'))
