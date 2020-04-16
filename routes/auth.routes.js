29cd3371d3cbc600bb69599c506b3e41c8114089
= <michael.w.murphy@outlook.com>
2020-04-16 14:45:55 +0100

    Added the authenticate_token route and added the password match verification to the login route, some refactoring

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -11,11 +11,23 @@ router.post('/register', async (req, res) => {
 
     // Validate the user
     const {error} = validation.registrationUserValidaion(req.body)
-    if (error) return res.status(400).send(error.details[0].message)
+    if (error) return res.status(400).send({
+        success: false,
+        msg: error.details[0].message
+    })
 
     // Does the user already exist
     const emailExists = await UserModel.findOne({ email: req.body.email })
-    if (emailExists) return res.status(400).send('A user with that email address already exists')
+    if (emailExists) return res.status(400).json({
+        success: false,
+        msg: 'An account with that email address already exists, try logging in'
+    })
+
+    // Ensure the passwords match
+    if (req.body.password !== req.body.password2) return res.status(400).json({
+        succes: false,
+        msg: 'The passwords provided do not match'
+    })
 
     // Hash the password
     const salt = await bcrypt.genSalt(10)
@@ -29,9 +41,16 @@ router.post('/register', async (req, res) => {
 
     try {
         const savedUser = await user.save()
-        res.send(savedUser)
+        res.json({
+            success: true,
+            user: savedUser
+        })
     } catch(err) {
-        res.status(400).send(err)
+        res.status(500).json({
+            success: false,
+            msg: 'It has not been possible to create an account at this time',
+            error: err
+        })
     }
 })
 
@@ -43,15 +62,75 @@ router.post('/login', async (req, res) => {
 
     // Does the user exist?
     const user = await UserModel.findOne({ email: req.body.email })
-    if (!user) return res.status(400).send('An account with that email address does not exist')
+    if (!user) return res.status(400).json({
+        success: false,
+        msg: 'An account with that email address does not exist'
+    })
 
     // Is the password correct?
     const validPassword = await bcrypt.compare(req.body.password, user.password)
-    if (!validPassword) return res.status(400).send('That password is incorrect')
+    if (!validPassword) return res.status(400).json({
+        success: false,
+        msg: 'That password is incorrect'
+    })
 
     // Create a JWT
     const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET)
-    res.header('auth-token', token).send(token)
+    res.header('auth-token', token).json({
+        success: true,
+        token: token,
+        user: {
+            name: user.name,
+            email: user.email
+        }
+    })
+})
+
+router.post('/authenticate_token', async (req, res) => {
+    const token = req.header('auth-token')
+
+    if (!token) return res.status(401).json({
+        success: false,
+        msg: 'Missing token'
+    })
+
+    try {
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET)
+
+        if (verified) {
+            const payload = token.split('.')[1]
+            const buff = new Buffer(payload, 'base64')
+            const userPayload = JSON.parse(buff.toString('ascii'))
+            const user = await UserModel.findById(userPayload._id)
+
+            if (!user) {
+                return res.status(404).json({
+                    success: false,
+                    msg: 'It has not been possible to locate a user with that id'
+                })
+            }
+
+            return res.json({
+                success: true,
+                user: {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email
+                } })
+        } else {
+            return res.status(401).json({
+                success: false,
+                msg: 'Bad auth'
+            })
+        }
+    } catch (err) {
+        console.error(err)
+
+        return res.status(401).json({
+            success: false,
+            msg: 'Bad authentication'
+        })
+    }
 })
 
 module.exports = router
